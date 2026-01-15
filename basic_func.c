4d23701b6e0e4460b66ed6db6e737224af9d6a5e
Matthew Burton <matthew.b2004@outlook.com>
2026-01-15 15:04:03 +0000

    forgot a file

--- basic_func.c
+++ basic_func.c
@@ -7,23 +7,21 @@
 int simple_minerva(double bunch_size){
     //random number first
     srand(time(NULL));
-    double threshold = rand()/(double)RAND_MAX;
+    double threshold = log(rand()/(double)RAND_MAX);
     int value = 0;
     double expectation = 0.81 * ((bunch_size)/2.25e13);
     double log_p;
-    double p;
     double log_sum = 0;
     //Then loop through possin value until the threshold is met:
     while(1){
         log_p = ((double)value*log(expectation)) - expectation + log_sum;
-        p = exp(log_p);
-        if(p > threshold)
+        if(log_p > threshold)
         {
             return value;
         }
         else
         {
-            threshold -=  p;
+            threshold -=  log_p;
             value++;
         }
         log_sum -= log(value);
